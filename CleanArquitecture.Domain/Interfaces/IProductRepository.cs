98f70858da254ea7a7a00fdb7290686e3c307ba4
Erwin <erwin.paralesda@gmail.com>
2022-10-11 18:24:00 -0500

    Add files via upload

---
+++ CleanArquitecture.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArquitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArquitecture.Domain.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+
+
+    }
+}
---
+++ CleanArquitecture.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArquitecture.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+
+        public Task Save();
+
+        public Task SaveAsync();
+
+        public Task <IEnumerable<T>> GetAll();
+
+        public Task <T>GetById(int id);
+
+        public Task Insert(T entity);
+
+        public Task Update(T entityUpdate);
+
+        public Task <bool>Exists(object id);
+
+        public Task Delete(object id);
+
+        public Task Delete(T entityDelete);
+
+
+    }
+}
